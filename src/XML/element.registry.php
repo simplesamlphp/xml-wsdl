9ceaf945c85915df5ca333cbdbc35c58a706b163
Tim van Dijen <tvdijen@gmail.com>
2024-10-08 14:56:07 +0200

    Update element registry

--- src/XML/element.registry.php
+++ src/XML/element.registry.php
@@ -6,4 +6,13 @@ return [
     'http://schemas.xmlsoap.org/wsdl/' => [
         'definitions' => '\SimpleSAML\WSSecurity\XML\wsdl\Definitions',
     ],
+    'http://schemas.xmlsoap.org/wsdl/soap12' => [
+        'address' => '\SimpleSAML\WSSecurity\XML\soap12\Address',
+        'binding' => '\SimpleSAML\WSSecurity\XML\soap12\Binding',
+        'body' => '\SimpleSAML\WSSecurity\XML\soap12\Body',
+        'fault' => '\SimpleSAML\WSSecurity\XML\soap12\Fault',
+        'header' => '\SimpleSAML\WSSecurity\XML\soap12\Header',
+        'headerfault' => '\SimpleSAML\WSSecurity\XML\soap12\HeaderFault',
+        'operation' => '\SimpleSAML\WSSecurity\XML\soap12\Operation',
+    ],
 ];
